e9faa658d16f67aec2d0da7692f6db9bd545aca0
kalillea <kalillea@stud.ntnu.no>
2024-01-24 10:52:05 +0100

    main branch

--- backend/src/firestore/firestore.ts
+++ backend/src/firestore/firestore.ts
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnc1CJVuxFAuNfEgaeGLYy-wXkUyEOKUA",
+  apiKey: "",
   authDomain: "backpacking-tdt4140.firebaseapp.com",
   databaseURL:
     "https://backpacking-tdt4140-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "backpacking-tdt4140",
   storageBucket: "backpacking-tdt4140.appspot.com",
-  messagingSenderId: "1070204029914",
-  appId: "1:1070204029914:web:1fc2f2a8b81bf022c04ee3",
-  measurementId: "G-E8V7RV259H",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
